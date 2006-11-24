0b6e5d1ad906614c502589cfc3859b4de80f9e75
roktas <roktas@788f1e2b-df1e-0410-8736-df70ead52e1b>
2006-11-24 16:59:18 +0000

    No need for an extra error message if mktemp fails; mktemp will already print an error.

--- branches/wrappers/src/wrappers/tempdir.sh
+++ branches/wrappers/src/wrappers/tempdir.sh
@@ -2,13 +2,9 @@
 pathfind mktemp || { err "Couldn't find 'mktemp'; aborting."; exit 1;  }
 
 THIS_TEMPDIR=
-readonly THIS_TEMPDIR="$(mktemp -d $THIS.XXXXXXXX)" || {
-    err "$THIS:  Couldn't create a temporary directory; aborting."
-    exit 1
-}
+THIS_TEMPDIR="$(mktemp -d $THIS.XXXXXXXX)" || exit 1
+readonly THIS_TEMPDIR
 
-trap '
-    exitcode=$?;
-    [ -z "$THIS_TEMPDIR" ] || rm -rf "$THIS_TEMPDIR"
-    exit $exitcode
-' 0 1 2 3 13 15
+trap 'exitcode=$?
+      [ -z "$THIS_TEMPDIR" ] || rm -rf "$THIS_TEMPDIR"
+      exit $exitcode' 0 1 2 3 13 15
